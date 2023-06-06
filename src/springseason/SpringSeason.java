8b159e604dc354e6e4ede58ab0d00c6296c72173
Krish2002876 <krishpatel11194@gmail.com>
2023-06-06 10:21:18 +0530

    first commit

---
+++ src/springseason/SpringSeason.java
@@ -0,0 +1,29 @@
+package springseason;
+
+public class SpringSeason {
+	public static void main(String[] args) {
+		spring(4, 5);
+		
+		
+	}
+
+	public static void spring(int m, int d) {
+		if (3 <= m <= 6) {
+			if (m = 3) && (20 <= d <= 31){
+				System.out.println("True"); 
+			}
+			else if (m = 4 && 5) && (1 <= d <= 30){
+				boolean true;
+			}
+			else if (m = 6) && (1 <= d <= 20){
+				boolean true;
+			}
+			else {
+				boolean false;
+			}	
+		}
+		else {
+			boolean false;
+		}
+	}
+}	
